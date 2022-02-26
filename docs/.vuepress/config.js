9a421a2eedd966ec1673e30218ccc3646c7723f3
trendtrading <lwjunun@gmail.com>
2022-02-26 17:38:45 +0800

    git init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    base: '/trendtrading/',
+    dest: 'dist',
+    title: '交易',
+    description: '交易',
+    themeConfig: {
+        logo: '/assets/img/interview.png',
+        displayAllHeaders: false,
+        editLinks: false,
+        sidebarDepth: 5,
+        docsDir: 'docs',
+        // header组件的菜单栏配置
+        nav: [
+            {
+                text: '交易',
+                link: '/c00-prepare/'
+            },
+            {
+                text: '书籍',
+                link: '/c01-book/'
+            },
+
+            {
+                text: '更多',
+                items: [
+                    {
+                        text: '书籍2',
+                        link: '/c01-book/'
+                    }
+                ]
+            },
+            {
+                text: 'GitHub',
+                link: 'https://gitee.com/trendtrading/trendtrading'
+            }
+        ],
+        // 侧边栏，多个侧边栏配置
+        sidebar: {
+            '/c00-prepare/': ['', 's01'],
+            '/c01-book/': ['']
+        }
+    }
+};
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroImage: /assets/img/interview.png
+actionText: 开始学习 →
+actionLink: /c00-prepare/
+---
